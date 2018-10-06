cd26c650f60bc4af2e957071c5aab63a9a96b562
ttllonsaikrisv <tllonsaikrisv@thetrainline.com>
2018-10-06 09:58:15 +0100

    set text to clipboard from SMS send, works only on emulators

--- src/test/java/com/appium/gesture/ClipboardTest.java
+++ src/test/java/com/appium/gesture/ClipboardTest.java
@@ -1,15 +1,27 @@
 package com.appium.gesture;
 
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
 import io.appium.java_client.clipboard.HasClipboard;
 import org.testng.annotations.Test;
 
-public class ClipboardTest extends BaseUserTest{
+public class ClipboardTest extends BaseUserTest {
 
     @Test
-    public void clipBoard() {
-        String text = "Selenium";
-        ((HasClipboard)driver).setClipboardText(text);
+    public void sendSMSandClipBoard() throws InterruptedException {
+        ((AndroidDriver) driver).sendSMS("555-555-5555", "Your message here!");
+        Thread.sleep(5000);
+        ((AndroidDriver<MobileElement>) driver).openNotifications();
+        Thread.sleep(5000);
+        String textFromSMS = driver.findElementById("android:id/big_text").getText();
+        ((HasClipboard) driver).setClipboardText(textFromSMS);
+        String clipboardText = ((HasClipboard) driver).getClipboardText();
+        ((AndroidDriver) driver).pressKey(new KeyEvent(AndroidKey.BACK));
         driver.findElementByAccessibilityId("username").clear();
-        driver.findElementByAccessibilityId("username").setValue(text);
+
+        driver.findElementByAccessibilityId("username").setValue(clipboardText);
+        Thread.sleep(5000);
     }
 }
