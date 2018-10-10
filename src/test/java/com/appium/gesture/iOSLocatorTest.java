26f6831349c8f283168ff409e089fe5b2d5dec40
saikrishna321 <saikrishna321@yahoo.com>
2018-10-10 21:11:33 +0100

    added more pattern examples for iosClassChain

--- src/test/java/com/appium/gesture/iOSLocatorTest.java
+++ src/test/java/com/appium/gesture/iOSLocatorTest.java
@@ -9,7 +9,9 @@ public class iOSLocatorTest extends BaseUserTest {
     @Test
     public void classChainTest() {
         login();
-        driver.findElement(MobileBy.iOSNsPredicateString("name CONTAINS 'Chained View'")).click();
+        //driver.findElement(MobileBy.iOSNsPredicateString("name IN {'chained VieW','Demos wheel picker color'} AND visible == 1")).click();
+        //driver.findElement(MobileBy.iOSNsPredicateString("name IN {'chained VieW','Chained View'}")).click();
+        driver.findElement(MobileBy.iOSNsPredicateString("name BEGINSWITH 'Chained'")).click();
         String text = driver.findElement(MobileBy.iOSClassChain("XCUIElementTypeWindow/**/XCUIElementTypeStaticText[2]")).getText();
         Assert.assertEquals(text,"Hello World, I'm View one ");
     }
