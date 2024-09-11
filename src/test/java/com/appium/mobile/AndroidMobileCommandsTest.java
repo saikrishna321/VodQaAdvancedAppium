22b944ec4ed21d76976a17514fa27f7789368f8c
saikrishna321 <saikrishna321@yahoo.com>
2024-09-11 16:20:23 +0530

    fix android test

--- src/test/java/com/appium/mobile/AndroidMobileCommandsTest.java
+++ src/test/java/com/appium/mobile/AndroidMobileCommandsTest.java
@@ -1,8 +1,10 @@
 package com.appium.mobile;
 
+import io.appium.java_client.AppiumBy;
 import io.appium.java_client.MobileBy;
 import io.appium.java_client.android.Activity;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.remote.AndroidMobileCapabilityType;
 import io.appium.java_client.remote.AutomationName;
 import io.appium.java_client.remote.MobileCapabilityType;
@@ -17,6 +19,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.time.Duration;
 import java.util.HashMap;
 
 import static org.junit.Assert.assertEquals;
@@ -30,35 +33,35 @@ public class AndroidMobileCommandsTest {
     @Before
     public void setUp() throws IOException {
         args = new HashMap<>();
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, AutomationName.ANDROID_UIAUTOMATOR2);
-        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Emulator");
-        capabilities.setCapability("adbExecTimeout", "70000");
-        capabilities.setCapability(MobileCapabilityType.APP, System.getProperty("user.dir") + "/ApiDemos.apk");
-        driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
-        wait  = new WebDriverWait(driver, 20);
+        UiAutomator2Options options = new UiAutomator2Options()
+                .setDeviceName("Android Emulator")
+                .setAutomationName("UiAutomator2")
+                .setApp(System.getProperty("user.dir") + "/apps/ApiDemos.apk");
+
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), options);
+        wait  = new WebDriverWait(driver, Duration.ofSeconds(20));
     }
 
     @Test
     public void setDateTest() {
         Activity activity = new Activity("io.appium.android.apis", ".view.DateWidgets1");
         driver.startActivity(activity);
-        wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.AccessibilityId("change the date"))).click();
-        WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.id("android:id/datePicker")));
+        wait.until(ExpectedConditions.presenceOfElementLocated(AppiumBy.accessibilityId("change the date"))).click();
+        WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(AppiumBy.id("android:id/datePicker")));
         args.put("year", "2020");
         args.put("monthOfYear", "10");
         args.put("dayOfMonth", "25");
         args.put("element", ((RemoteWebElement) element).getId());
         driver.executeScript("mobile: setDate", args);
         String text = wait.until(ExpectedConditions.presenceOfElementLocated(
-                MobileBy.id("android:id/date_picker_header_date"))).getText();
+                AppiumBy.id("android:id/date_picker_header_date"))).getText();
         assertEquals("Sun, Oct 25", text);
     }
 
     @Test
     public void setTimeTest() {
         Activity activity = new Activity("io.appium.android.apis", ".view.DateWidgets2");
-        driver.startActivity(activity);
+        (AndroidDriver) driver.startActivity(activity);
         WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.className("android.widget.TimePicker")));
         args.put("hours", "11");
         args.put("minutes", "0");
